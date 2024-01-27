2be1dc65cbc7c3f67bf47458e46c3202a6663ab8
bimaatriadmaja <bimaatriadmaja17@gmail.com>
2024-01-27 08:55:10 +0700

    First commit

---
+++ Modul 1/Identitas.java
@@ -0,0 +1,9 @@
+public class Identitas {
+  public static void main(String [] args){
+    System.out.println("Bima Triadmaja");
+    System.out.println("L200210137");
+    System.out.println("Saya akan lulus kuliah daring ini" + "\n" +
+                      "Semoga saya (Bima Triadmaja) berjodoh dengan Arzad Lintang Maharani" + "\n" +
+                      "Aamiin :)");
+  }
+}
